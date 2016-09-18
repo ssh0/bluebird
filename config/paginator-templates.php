c87454c1eae0568dd4dee6c42266c233ec683514
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2016-09-18 21:30:47 +0900

    Modify pagination layout

--- config/paginator-templates.php
+++ config/paginator-templates.php
@@ -1,11 +1,11 @@
 <?php
 return [
-    'current' => ' {{text}} ',
-    'number' => ' <a href="{{url}}">{{text}}</a> ',
-    'first' => ' <a href="{{url}}">{{text}}</a> ',
-    'last' => ' <a href="{{url}}">{{text}}</a> ',
-    'nextActive' => '<a rel="next" href="{{url}}">次のページ</a>',
-    'nextDisabled' => '',
-    'prevActive' => '<a rel="prev" href="{{url}}">前のページ</a>',
-    'prevDisabled' => '',
+    'current' => '{{text}}',
+    'number' => '<a href="{{url}}">{{text}}</a>',
+    'first' => '<div class="page_first"><a href="{{url}}">{{text}}</a></div>',
+    'last' => '<div class="page_last"><a href="{{url}}">{{text}}</a></div>',
+    'nextActive' => '<div class="page_next"><a rel="next" href="{{url}}">次のページ</a></div>',
+    'nextDisabled' => '<div class="page_next_disabled">次のページ</div>',
+    'prevActive' => '<div class="page_prev"><a rel="prev" href="{{url}}">前のページ</a></div>',
+    'prevDisabled' => '<div class="page_prev_disabled">前のページ</div>',
 ];
